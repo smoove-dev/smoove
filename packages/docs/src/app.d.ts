23b10f3100394b086b31db6549c62231231ee22f
shemi <shemi.perez@gmail.com>
2026-07-08 09:43:29 +0300

    fix

--- packages/docs/src/app.d.ts
+++ packages/docs/src/app.d.ts
@@ -3,7 +3,5 @@ import "@react-router/node"; // or the adapter you are using
 
 declare module "react-router" {
   // Augment the existing AppLoadContext interface
-  interface AppLoadContext {
-
-  }
+  interface AppLoadContext {}
 }
